1167b469ccbe25fc5415f9c04c13338d8860fd3a
JM Ramos <jramos@zendesk.com>
2018-08-19 10:24:50 +0800

    Add seeder for room types

--- database/seeds/RoomTypesSeeder.php
+++ database/seeds/RoomTypesSeeder.php
@@ -13,9 +13,28 @@ class RoomTypesSeeder extends Seeder
     public function run()
     {
         RoomType::create([
-            'name' => 'Studio Apartment',
-            'description' => 'A simple studio apartment. Good for solo travellers',
-            'url' => url(''),
+            'name' => 'Standard Room',
+            'description' => 'Air-conditioned, Queen sized bed, Hot and Cold shower, TV with Cable, Complimentary Use of the swimming pool,
+Free Wifi',
+            'url' => url('https://occupancy.plus/images/rooms/6e74766b5cfaced732955a8f1d857f70.jpg'),
+        ]);
+
+        RoomType::create([
+            'name'  => 'Standard Room Twin Bed',
+            'description' => 'Air conditioned, hot and cold shower, free Wifi, swimming pool, and cable T.V.  twin bed',
+            'url' => url('https://occupancy.plus/images/rooms/f6a848d482ad64b01d4091fb478ec616.jpg'),
+        ]);
+
+        RoomType::create([
+            'name'  => 'Fan Room',
+            'description' => '1 Double sized bed, Fan ventilated, bathroom with hot and cold shower	2',
+            'url' => url('https://occupancy.plus/images/rooms/6aa91d6eb855f24f2da5362d99ee37ab.jpg'),
+        ]);
+
+        RoomType::create([
+            'name'  => 'Studio Type',
+            'description' => 'Air conditioned, hot and cold shower, free Wifi, swimming pool, and cable T.V.  Double bed',
+            'url' => url('https://occupancy.plus/images/rooms/e382052d9e0b7d17f3e56c698ff3948e.jpg'),
         ]);
     }
 }
