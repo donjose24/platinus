a84b0f20b9d255baeea1f111b1e1b5d84fc29b22
JM Ramos <jramos@zendesk.com>
2018-08-26 08:45:36 +0800

    Fix redirection

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -35,5 +36,14 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function authenticated(Request $request, $user)
+    {
+        if ($user->hasRole("customer")) {
+            $this->redirectTo = "/customer";
+        } else if ($user->hasRole("cashier")) {
+            $this->redirectTo = "/cashier";
+        }
     }
 }
