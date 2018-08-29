eeae22c2e8ae7bf537265c903534307a811112a9
Joshua Turingan <jotu@splitmedialabs.com>
2018-08-30 00:16:26 +0800

    add footer skeletal

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,42 @@
+<footer>
+  <div class="pt-4 pb-4">
+    <div class="footer-content">
+      <div class="d-flex justify-content-between">
+        <div class="footer-content-block p-4">
+          <h3 class="mb-3">WHY BOOK WITH US?</h3>
+          <ul class="reason-block">
+            <li>Exclusive offers only on our website</li>
+            <li>Lowest Rate Guaranteed</li>
+            <li>Instant Confirmation Online</li>
+            <li>Free support before and after booking</li>
+          </ul>
+        </div>
+        <div class="footer-content-block p-4">
+          <h4>PAYMENTS ACCEPTED</h4>
+          <ul class="image-container">
+            <li><img src="" alt=""></li>
+            <li><img src="" alt=""></li>
+            <li><img src="" alt=""></li>
+            <li><img src="" alt=""></li>
+          </ul>
+          <h4>PAYMENTS SECURED BY</h4>
+          <ul class="image-container">
+            <li><img src="" alt=""></li>
+            <li><img src="" alt=""></li>
+            <li><img src="" alt=""></li>
+            <li><img src="" alt=""></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="footer-nav">
+    <ul class="d-flex justify-content-between w-50 m-auto">
+      <li><a href="/">Home</a></li>
+      <li><a href="/">Rooms &amp; Rates</a></li>
+      <li><a href="/">Services &amp; Facilities</a></li>
+      <li><a href="/location">Location</a></li>
+      <li><a href="/contact">Contact Us</a></li>
+    </ul>
+  </div>
+</footer>
