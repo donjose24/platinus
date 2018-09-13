c71ec406476c1dc992c644f3a4cda2aad18a3b80
Joshua Turingan <jotu@splitmedialabs.com>
2018-09-13 23:52:56 +0800

    update fix for the deposit slip and design for reserve now

--- resources/views/customer/booking/reservation.blade.php
+++ resources/views/customer/booking/reservation.blade.php
@@ -1,23 +1,17 @@
 @extends('layouts.customer')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">Reservation Now</div>
-                    <div class="card-body">
-                        <div class="reservation-container">
-                            {{ Form::open(['url' => '/room/search', 'method' => 'get']) }}
-                            <li><label class="d-block" for="start_date">Start Date</label><input type="text" name="start_date" value="" placeholder="From" class="datetime-picker" /></li>
-                            <li><label class="d-block" for="end_date">End Date</label><input type="text" name="end_date" placeholder="To" value="" class="datetime-picker" /></li>
-                            <li><label class="d-block" for="adults">Number of Persons</label><input readonly type="number" name="adults" value="" class="no-guest spinner" /></li>
-                            <li><button class="btn btn-custom-default w-100">Book Now</button></li>
-                            {{ Form::close() }}
-                        </div>
-                    </div>
-                </div>
+    <h2 class="font-weight-bold">Reserve Now</h2>
+    {{ Form::open(['url' => '/room/search', 'method' => 'get']) }}
+    <div class="card">
+        <div class="card-body">
+            <div class="reservation-container reserve-now d-flex justify-content-between">
+                <div class="w-25 d-flex flex-column justify-content-between"><label class="d-block" for="start_date">Start Date</label><input type="text" name="start_date" value="" placeholder="From" class="w-100 datetime-picker" /></div>
+                <div class="w-25 d-flex flex-column justify-content-between"><label class="d-block" for="end_date">End Date</label><input type="text" name="end_date" placeholder="To" value="" class="w-100 datetime-picker" /></div>
+                <div class="w-25"><label class="d-block" for="adults">Number of Persons</label><input readonly type="number" min="0" name="adults" value="" class="no-guest spinner" /></div>
             </div>
         </div>
     </div>
+    <div class="text-right mt-3"><button class="btn btn-custom-default w-25">Book Now</button></div>
+    {{ Form::close() }}
 @endsection
