ffafe49767ec21dca92bf347086cc8326b0ae79f
JM Ramos <jramos@zendesk.com>
2018-09-26 11:28:28 +0800

    Fix missing dates in search bar

--- resources/views/search-bar.blade.php
+++ resources/views/search-bar.blade.php
@@ -1,7 +1,7 @@
 {{Form::open(['url' => '/room/search', 'method' => 'get'])}}
 <ul>
-    <li><label class="d-block" for="start_date">Start Date</label><input readonly type="text" name="start_date"  placeholder="From" class="datetime-picker" /></li>
-    <li><label class="d-block" for="end_date">End Date</label><input readonly type="text" name="end_date" placeholder="To" class="datetime-picker" /></li>
+    <li><label class="d-block" for="start_date">Start Date</label><input readonly type="text" name="start_date"  value="{{ app('request')->input('start_date') }}" placeholder="From" class="datetime-picker" /></li>
+    <li><label class="d-block" for="end_date">End Date</label><input readonly type="text" name="end_date" value="{{ app('request')->input('end_date') }}"placeholder="To" class="datetime-picker" /></li>
     <li><button class="btn btn-custom-default w-100">Book Now</button></li>
 </ul>
 @if ($errors->any())
