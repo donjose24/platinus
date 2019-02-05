550d12685b6b5a4e2c7c34b01c94de9dc75a517e
jmramos02 <ramosjosemari@gmail.com>
2019-02-05 13:32:45 +0800

    Update seeders

--- database/seeds/RoomTypesSeeder.php
+++ database/seeds/RoomTypesSeeder.php
@@ -14,7 +14,7 @@ class RoomTypesSeeder extends Seeder
     public function run()
     {
         RoomType::create([
-            'name' => 'Standard Room',
+            'name' => 'Studio Twin Room',
             'description' => 'Air-conditioned, Queen sized bed, Hot and Cold shower, TV with Cable, Complimentary Use of the swimming pool,
 Free Wifi',
             'image_url' => 'https://occupancy.plus/images/rooms/6e74766b5cfaced732955a8f1d857f70.jpg',
@@ -24,44 +24,12 @@ Free Wifi',
         ]);
 
         RoomType::create([
-            'name'  => 'Standard Room Twin Bed',
+            'name'  => 'Studio Twin Room',
             'description' => 'Air conditioned, hot and cold shower, free Wifi, swimming pool, and cable T.V.  twin bed',
             'image_url' => 'https://occupancy.plus/images/rooms/f6a848d482ad64b01d4091fb478ec616.jpg',
             'daily_rate' => 2000,
             'weekly_rate' => 12000,
             'capacity' => 2,
-        ]);
-
-        RoomType::create([
-            'name'  => 'Fan Room',
-            'description' => '1 Double sized bed, Fan ventilated, bathroom with hot and cold shower	2',
-            'image_url' => 'https://occupancy.plus/images/rooms/6aa91d6eb855f24f2da5362d99ee37ab.jpg',
-            'daily_rate' => 950,
-            'weekly_rate' => 4500,
-            'capacity' => 2,
-        ]);
-
-        RoomType::create([
-            'name'  => 'Single Room',
-            'description' => 'Air conditioned, hot and cold shower, free Wifi, swimming pool, and cable T.V.  Double bed',
-            'image_url' => 'https://occupancy.plus/images/rooms/e382052d9e0b7d17f3e56c698ff3948e.jpg',
-            'daily_rate' => 2500,
-            'weekly_rate' => 16000,
-            'capacity' => 2,
-        ]);
-
-        Room::create([
-            'number'  => '401',
-            'room_type_id' => '31',
-            'status' => 'ready',
-            'note' => '',
-        ]);
-
-        Room::create([
-            'number'  => '405',
-            'room_type_id' => '31',
-            'status' => 'ready',
-            'note' => '',
         ]);
 
         Room::create([
@@ -88,20 +56,6 @@ Free Wifi',
         Room::create([
             'number'  => '102',
             'room_type_id' => '11',
-            'status' => 'ready',
-            'note' => '',
-        ]);
-
-        Room::create([
-            'number'  => '103',
-            'room_type_id' => '41',
-            'status' => 'ready',
-            'note' => '',
-        ]);
-
-        Room::create([
-            'number'  => '104',
-            'room_type_id' => '41',
             'status' => 'ready',
             'note' => '',
         ]);
