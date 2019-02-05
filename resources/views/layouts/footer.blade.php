51d1860f0c6265c729da403094bda6dcd031d233
jmramos02 <ramosjosemari@gmail.com>
2019-02-05 14:18:25 +0800

    Remove footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,35 +1,4 @@
 <footer>
-  <div class="pt-4 pb-4">
-    <div class="footer-content">
-      <div class="d-flex justify-content-center">
-        <div class="footer-content-block mr-4 p-4">
-          <h2 class="mb-3">WHY BOOK WITH US?</h2>
-          <ul class="reason-block">
-            <li class="d-flex align-items-center">
-              <span class="mr-2"><i class="fa fa-check-circle"></i></span>
-              <span class="font-weight-bold">Exclusive offers only on our website</span>
-            </li>
-            <li class="d-flex align-items-center">
-              <span class="mr-2"><i class="fa fa-check-circle"></i></span>
-              <span class="font-weight-bold">Lowest Rate Guaranteed</span>
-            </li>
-            <li class="d-flex align-items-center">
-              <span class="mr-2"><i class="fa fa-check-circle"></i></span>
-              <span class="font-weight-bold">Instant Confirmation Online</span>
-            </li>
-            <li class="d-flex align-items-center">
-              <span class="mr-2"><i class="fa fa-check-circle"></i></span>
-              <span class="font-weight-bold">Free support before and after booking</span>
-            </li>
-          </ul>
-        </div>
-        <div class="footer-content-block p-4">
-          <h4>PAYMENTS ACCEPTED</h4>
-          <p class="w-50 m-auto"><img class="w-100" src="/images/payments/eastwest.png" alt="East West Bank"></p>
-        </div>
-      </div>
-    </div>
-  </div>
   <div class="footer-nav">
     <ul class="d-flex justify-content-between w-50 m-auto">
       <li><a href="/">Home</a></li>
