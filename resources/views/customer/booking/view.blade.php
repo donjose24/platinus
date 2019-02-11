0e6342c291586432b5b9761dd8c60223d837d964
jmramos02 <ramosjosemari@gmail.com>
2019-02-11 21:22:43 +0800

    Add Cancel button

--- resources/views/customer/booking/view.blade.php
+++ resources/views/customer/booking/view.blade.php
@@ -44,7 +44,9 @@
                 @endphp
                 <h4> VAT: PHP {{ number_format($taxAmount, 2) }}</h4>
                 <h4>Grand Total: PHP {{ number_format($total, 2) }}</h4>
-                <button href="#" class="btn btn-secondary cancel">Cancel</button>
+                @if($reservation->status == "waiting_for_approval" || $reservation->status == "approved")
+                    <a href="#" class="btn btn-danger p-2 w-25 cancelReservation" style="color:white"> Cancel Reservation </a>
+                @endif
             </fieldset>
             <br>
             <br>
