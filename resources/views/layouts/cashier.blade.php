9b36d693c5b982f80ac8a5b9f12016d2a49c0a26
jmramos02 <ramosjosemari@gmail.com>
2019-02-11 21:43:31 +0800

    Remove invalid jquery

--- resources/views/layouts/cashier.blade.php
+++ resources/views/layouts/cashier.blade.php
@@ -54,7 +54,6 @@
 </div>
 
 <!-- Scripts -->
-<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
