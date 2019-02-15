728374b0dafedaa0ec38b902ab9b836f6c617bbc
jmramos02 <ramosjosemari@gmail.com>
2019-02-15 23:35:18 +0800

    Admin fixes

--- resources/views/admin/reservations/reservations/show.blade.php
+++ resources/views/admin/reservations/reservations/show.blade.php
@@ -39,12 +39,56 @@
                     @endforeach
                 </table>
                 @php
+                    $totalPaid = 0;
+                @endphp
+                <h1 class="mb-3">Transactions</h1>
+                @if(count($reservation->transactions()->get()) != 0)
+                    @foreach($reservation->transactions()->get() as $transaction)
+                        <div class="card w-100 mb-0">
+                            <div class="card-body d-flex p-0">
+                                <div class="w-25 p-3 border-right border-bottom">
+                                    <p class="mb-2">Item</p>
+                                    <p class="mb-0 font-weight-bold">{{ $transaction->item }}</p>
+                                </div>
+                                <div class="w-25 p-3 border-right border-bottom">
+                                    <p class="mb-2">Price</p>
+                                    <p class="mb-0 font-weight-bold">{{ number_format($transaction->price, 2) }}</p>
+                                </div>
+                                <div class="w-25 p-3 border-bottom border-right">
+                                    <p class="mb-2">Date Added</p>
+                                    <p class="mb-0 font-weight-bold">{{ date_format($transaction->created_at, "F d, Y h:iA") }}</p>
+                                </div>
+                                <div class="w-25 p-3 border-bottom">
+                                    @if($transaction->item != "Bank Deposit")
+                                        <p class="mb-2">Actions</p>
+                                        @if($transaction->status != "paid")
+                                            <a href="/cashier/reservation/settle/{{ $transaction->id }}" class="btn btn-success">Settle</a>
+                                        @endif
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                        @php
+                            if($transaction->item != "Bank Deposit" && $transaction->item != "Balance Payment") {
+                                $total += $transaction->price;
+                            }
+                            if ($transaction->status == "paid") {
+                                $totalPaid += $transaction->price;
+                            }
+                        @endphp
+                    @endforeach
+                @else
+                    No transactions yet!
+                @endif
+
+                @php
                     $tax = setting('tax');
                     $taxAmount = $total * ($tax / 100);
                     $total += $total * ($tax / 100);
                 @endphp
                 <h4> VAT: PHP {{ $taxAmount }}</h4>
                 <h4>Grand Total: PHP {{ number_format($total, 2) }}</h4>
+                <h4> Total Paid {{ number_format($totalPaid, 2) }}</h4>
             </fieldset>
             <br>
             <br>
