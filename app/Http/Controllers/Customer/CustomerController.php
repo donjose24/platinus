7b363bef0eeb65621610e054d0002552aa0de063
jmramos02 <ramosjosemari@gmail.com>
2019-02-15 23:10:53 +0800

    Fix total pay

--- app/Http/Controllers/Customer/CustomerController.php
+++ app/Http/Controllers/Customer/CustomerController.php
@@ -8,7 +8,9 @@ use App\Reservation;
 use App\ReservationRoom;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
 use Imgur;
 use App\Helpers\ReservationHelper;
 
@@ -125,5 +127,19 @@ class CustomerController extends Controller
         }
 
         return view('cashier.rebook', compact('reservation', 'roomsWillBeRemoved'));
+    }
+
+    public function changePassword(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $user = Auth::user();
+        $user->password = Hash::make($request->get('password'));
+        $user->save();
+
+        \session()->flash('flash_message', 'Success');
     }
 }
