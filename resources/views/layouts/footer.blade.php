faddc83c530034c2b6789c6ff53ad00037e97d6b
jmramos02 <ramosjosemari@gmail.com>
2019-08-09 20:31:21 +0800

    Fix footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,8 +2,8 @@
   <div class="footer-nav">
     <ul class="d-flex justify-content-between w-50 m-auto">
       <li><a href="/">Home</a></li>
-      <li><a href="/">Rooms &amp; Rates</a></li>
-      <li><a href="/">Services &amp; Facilities</a></li>
+      <li><a href="/roomrates">Rooms &amp; Rates</a></li>
+      <li><a href="/facilities">Services &amp; Facilities</a></li>
       <li><a href="/location">Location</a></li>
       <li><a href="/contact">Contact Us</a></li>
     </ul>
