172bcd84d5163e4810731a0a78e4cdaa435ad060
jmramos02 <ramosjosemari@gmail.com>
2019-10-05 18:27:04 +0800

    Fix default value for seeders

--- database/seeds/RoomTypesSeeder.php
+++ database/seeds/RoomTypesSeeder.php
@@ -14,20 +14,19 @@ class RoomTypesSeeder extends Seeder
     public function run()
     {
         RoomType::create([
-            'name' => 'Studio Twin Room',
-            'description' => 'Air-conditioned, Queen sized bed, Hot and Cold shower, TV with Cable, Complimentary Use of the swimming pool,
-Free Wifi',
-            'image_url' => 'https://occupancy.plus/images/rooms/6e74766b5cfaced732955a8f1d857f70.jpg',
-            'daily_rate' => 1600,
+            'name' => 'Studio Single Room',
+            'description' => 'An Air-Conditioned Room with One Double-Sized Bed, Flat-Screen TV with cable, Free WiFi and Private Bathroom.',
+            'image_url' => 'https://i.imgur.com/SUxe3jD.jpg',
+            'daily_rate' => 3000,
             'weekly_rate' => 10500,
             'capacity' => 2,
         ]);
 
         RoomType::create([
-            'name'  => 'Studio Twin Room Deluxe',
-            'description' => 'Air conditioned, hot and cold shower, free Wifi, swimming pool, and cable T.V.  twin bed',
+            'name'  => 'Studio Twin Room',
+            'description' => 'An Air-Conditioned Room with Two Double-Sized Bed, Flat-Screen TV with cable, Dining Area, Kitchenette, Free WiFi and Private Bathroom.',
             'image_url' => 'https://occupancy.plus/images/rooms/f6a848d482ad64b01d4091fb478ec616.jpg',
-            'daily_rate' => 2000,
+            'daily_rate' => 3200,
             'weekly_rate' => 12000,
             'capacity' => 2,
         ]);
