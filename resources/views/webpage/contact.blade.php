68e831804ddc36029204e3a8a4f94da6fd7ecf19
jmramos02 <ramosjosemari@gmail.com>
2019-10-05 18:35:57 +0800

    Add map to contact us page

--- resources/views/webpage/contact.blade.php
+++ resources/views/webpage/contact.blade.php
@@ -23,6 +23,7 @@ Mabalacat, Pampanga</h5>
                 <h2>Email Address</h2>
                 <h5>hotelplatanusgmainc@gmail.com</h5>
                 <br>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3850.671678370386!2d120.51420381538117!3d15.176365166774335!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33968d5827f57a21%3A0xe0155f15c9a813c8!2sPlatanus%20Hotel!5e0!3m2!1sen!2sph!4v1570271636662!5m2!1sen!2sph" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
             </div>
             <div class="col-md-6 col-sm-12 mt-2">
                 <div class="contact-instruction p-3">
